6d4f11fbdfbd6ee3e7cb36af327828f278cdf79c
Roger Zhuang <zhuangziyin@gmail.com>
2022-11-09 13:29:32 -0800

    do / while practice

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,18 @@
 for (let i=1; i< 11; i++) {
-    console.log(Math.pow(2,i));
+    console.log(Math.pow(2,i)); //2**i
 }
 
 let j = 1
 while (j<11) {
-    console.log(Math.pow(2,j));
+    console.log(Math.pow(2,j)); //2**i
     j++;
 }
+
+let input="";
+input = prompt("what is your name?");
+while (!(isNaN(Number(input)))) {
+    alert("please input a name")
+    input = prompt("what is your name?");
+}
+
+alert(`Hello ${input}`);
