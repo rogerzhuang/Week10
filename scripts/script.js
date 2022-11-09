d4e91140a9c7dcbd340fe26e8cdf93cdadb77649
Roger Zhuang <zhuangziyin@gmail.com>
2022-11-09 14:10:41 -0800

    obj practice

--- scripts/script.js
+++ scripts/script.js
@@ -32,3 +32,45 @@ for (let i=0; i<fruits.length; i++) {
 for (let item of fruits) {
     console.log(item);
 }
+
+
+const person = {
+    name:['Bob', 'Smith'],
+    age:32,
+    gender:'male',
+    hobbies :['music', 'skiing']
+}
+for (let key in person) {
+    console.log(key);
+    console.log(person[key]);
+}
+console.log(`my name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.hobbies[0]} and ${person.hobbies[1]}`)
+
+
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+};
+
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+};
+
+let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+};
+let students = [student1, student2, student3]; 
+
+let i = 0
+for (let student of students) {
+    if (student.program === "CIT" && student.GPA > 3.0) {
+        console.log(student);
+        i++;
+    }
+}
+console.log(i);
